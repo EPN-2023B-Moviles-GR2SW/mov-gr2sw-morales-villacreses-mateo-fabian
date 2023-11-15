149905db39678cee8b353fb013c56bdb95c19ce6
Mateo Morales <vimofama@outlook.com>
2023-11-15 09:59:13 -0500

    Add variables and functions for calculating salary

--- 01-KOTLIN/proyecto/src/main/kotlin/Main.kt
+++ 01-KOTLIN/proyecto/src/main/kotlin/Main.kt
@@ -1,7 +1,140 @@
+import java.util.Date
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+
+    // INMUTABLES (NO se reasignan "=")
+    val inmutable: String = "Mateo";
+
+    // MUTABLES (Re asignar)
+    var mutable: String = "Mateo";
+    mutable = "Fabian";
+
+    println("INMUTABLE: $inmutable");
+    println("MUTABLE: $mutable");
+
+    // val > var
+    // Duck Typing
+    var ejemploVariable = " Mateo Morales "
+    val edadEjemplo: Int = 12
+    ejemploVariable.trim()
+    // ejemploVariable = edadEjemplo; //!error
+
+    // Variables primitiva
+    val nombreProfesor: String = "Adrian Eguez"
+    val sueldo: Double = 1.2
+    val estadoCivil: Char = 'C'
+    val mayorEdad: Boolean = true
+    // Clases Java
+    val fechaNacimiento: Date = Date()
+
+    // SWITCH
+    val estadoCivilWhen = "C"
+    when (estadoCivilWhen) {
+        ("C") -> {
+           println("Casado")
+        }
+        "S" -> {
+            println("Soltero")
+        }
+        else -> {
+            println("No sabemos")
+        }
+    }
+
+    val esSoltero = (estadoCivilWhen == "S")
+    val coqueto = if (esSoltero) "Si" else "No"
+
+    calcularSueldo(10.00)
+    calcularSueldo(10.00,12.00, 20.00)
+    calcularSueldo(10.00, bonoEspecial = 20.00) // Named Parameters
+    calcularSueldo(bonoEspecial = 20.00, sueldo = 10.00, tasa = 14.00) // Parametros nombrados
+
+    val sumaUno = Suma(1, 1)
+    val sumaDos = Suma(null, 1)
+    val sumaTres = Suma(1, null)
+}
+
+abstract class NumerosJava{
+    protected val numeroUno: Int
+    private val numeroDos: Int
+
+    constructor(
+        uno: Int,
+        dos: Int
+    ){ // Bloque de codigo del constructor
+        this.numeroUno = uno
+        this.numeroDos = dos
+        println("Inicializando")
+    }
+}
+
+abstract class Numeros(
+    // Ejemplo:
+    // uno: Int, (Parametro (sin modificador de acceso))
+    // private var uno: Int, // Propiedad Clase numeros.uno
+    // var uno: Int, // Propiedad de la clase (por defecto es PUBLIC)
+    // public var uno: Int,
+    protected val numeroUno: Int, // Propiedad de la case protected numeros.numeroUno
+    protected val numeroDos: Int, // Propiedad de la case protected numeros.numeroDos
+){
+    // var cedula: string = "" (public es por defecto)
+    // private valorCalculado: Int = 0 (private)
+    init { // bloque constructor primario
+        this.numeroUno; this.numeroDos; // this es opcional
+        numeroUno; numeroDos // sin el "this", es lo mismo
+        println("Inicializando")
+    }
+}
+
+
+class Suma( // Constructor Primario Suma
+    unoParametro: Int, // Parametro
+    dosParametro: Int, // Parametro
+): Numeros(unoParametro, dosParametro){ // Extendiendo y mandando los parametros (super)
+    init { // Bloque codigo constructor primario
+        this.numeroDos
+        this.numeroDos
+    }
+
+    constructor(// Segundo constructor
+        uno: Int?, // Parametros
+        dos: Int, // Parametros
+    ): this(
+        if (uno == null) 0 else uno,
+        dos
+    )
+
+    constructor(// Tercer constructor
+        uno: Int, // Parametros
+        dos: Int?, // Parametros
+    ): this(
+        uno,
+        if (dos == null) 0 else dos,
+    )
+}
+
+// Void -> Unit
+fun imprimirNombre(nombre: String): Unit {
+    println("Nombre: ${nombre}") // template strings
+}
+
+
+fun calcularSueldo(
+    sueldo: Double, // Requerido
+    tasa: Double = 12.00, // Opcional (defecto)
+    bonoEspecial: Double? = null, //Opcion null -> nullable
+): Double{
+    // Int -> Int? (nullable)
+    // String -> String? (nullable)
+    // Date -> Date? (nullable)
+    if (bonoEspecial == null){
+        return sueldo * (100/tasa)
+    } else {
+        return sueldo * (100/tasa) + bonoEspecial
+    }
 }
