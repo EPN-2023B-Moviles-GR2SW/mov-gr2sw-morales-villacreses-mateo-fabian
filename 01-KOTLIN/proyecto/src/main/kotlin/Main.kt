84436ee8c2772e15f6b26c45ce6e172bcd3b4ec0
Mateo Morales <vimofama@outlook.com>
2023-11-21 09:07:06 -0500

    Fix formatting and add array operations

--- 01-KOTLIN/proyecto/src/main/kotlin/Main.kt
+++ 01-KOTLIN/proyecto/src/main/kotlin/Main.kt
@@ -36,11 +36,13 @@ fun main(args: Array<String>) {
     val estadoCivilWhen = "C"
     when (estadoCivilWhen) {
         ("C") -> {
-           println("Casado")
+            println("Casado")
         }
+
         "S" -> {
             println("Soltero")
         }
+
         else -> {
             println("No sabemos")
         }
@@ -50,23 +52,87 @@ fun main(args: Array<String>) {
     val coqueto = if (esSoltero) "Si" else "No"
 
     calcularSueldo(10.00)
-    calcularSueldo(10.00,12.00, 20.00)
+    calcularSueldo(10.00, 12.00, 20.00)
     calcularSueldo(10.00, bonoEspecial = 20.00) // Named Parameters
     calcularSueldo(bonoEspecial = 20.00, sueldo = 10.00, tasa = 14.00) // Parametros nombrados
 
     val sumaUno = Suma(1, 1)
     val sumaDos = Suma(null, 1)
     val sumaTres = Suma(1, null)
+    val sumaCuatro = Suma(null, null)
+    sumaUno.sumar()
+    sumaDos.sumar()
+    sumaTres.sumar()
+    sumaCuatro.sumar()
+    println("Numero pi = ${Suma.pi}")
+    println("2^2 = ${Suma.elevarAlCuadrado(2)}")
+    println("Historial de sumas: ${Suma.historialSumas}")
+
+    println("\n############# ARREGLOS #############")
+    // Arreglo Estatico
+    val arregloEstatico: Array<Int> = arrayOf<Int>(1, 2, 3)
+    println(arregloEstatico.contentToString()) // println(arregloEstatico) imprime la direccion de memoria
+
+    // Arreglo Dinamico
+    val arregloDinamico: ArrayList<Int> = arrayListOf<Int>(
+        1,2,3,4,5,6,7,8,9,10
+    )
+    println(arregloDinamico)
+    arregloDinamico.add(11)
+    arregloDinamico.add(12)
+    println(arregloDinamico)
+
+    // FOR EACH -> Unit
+    // Iterar un arreglo
+    val respuestaForEach: Unit = arregloDinamico.forEach{ valorActual: Int ->
+        println("Valor actual: ${valorActual}")
+    }
+    // it (en ingles eso) significa el elemento iterado
+    arregloDinamico.forEach{ println(it) }
+
+    arregloEstatico
+        .forEachIndexed { indice: Int, valorActual: Int ->
+            println("Valor ${valorActual} Indice: ${indice}")
+        }
+    println(respuestaForEach)
+
+    // MAP -> Muta el arreglo (Cambia el arreglo)
+    // 1) Enviamos el nuevo valor de la iteracion
+    // 2) Nos devuelve un NUEVO ARREGLO con los valores modificados
+
+    val respuestaMap: List<Double> = arregloDinamico
+        .map { valorActual: Int ->
+            return@map valorActual.toDouble() + 100.00
+        }
+    println(respuestaMap)
+
+    val respuestaMapDos = arregloDinamico.map { it + 15 }
+    println(respuestaMapDos)
+
+    // Filter -> FILTRAR EL ARREGLO
+    // 1) Devolver una expresion TRUE o FALSE
+    // 2) Nuevo arreglo filtrado
+    val respuestaFilter: List<Int> = arregloDinamico
+        .filter { valorActual: Int ->
+            // Expresion Condicion
+            val mayoresACinco: Boolean = valorActual > 5
+            return@filter mayoresACinco
+        }
+
+    val respuestaFilterDos = arregloDinamico.filter { it <= 5 }
+
+    println(respuestaFilter)
+    println(respuestaFilterDos)
 }
 
-abstract class NumerosJava{
+abstract class NumerosJava {
     protected val numeroUno: Int
     private val numeroDos: Int
 
     constructor(
         uno: Int,
         dos: Int
-    ){ // Bloque de codigo del constructor
+    ) { // Bloque de codigo del constructor
         this.numeroUno = uno
         this.numeroDos = dos
         println("Inicializando")
@@ -81,7 +147,7 @@ abstract class Numeros(
     // public var uno: Int,
     protected val numeroUno: Int, // Propiedad de la case protected numeros.numeroUno
     protected val numeroDos: Int, // Propiedad de la case protected numeros.numeroDos
-){
+) {
     // var cedula: string = "" (public es por defecto)
     // private valorCalculado: Int = 0 (private)
     init { // bloque constructor primario
@@ -92,30 +158,65 @@ abstract class Numeros(
 }
 
 
-class Suma( // Constructor Primario Suma
+class Suma(
+    // Constructor Primario Suma
     unoParametro: Int, // Parametro
     dosParametro: Int, // Parametro
-): Numeros(unoParametro, dosParametro){ // Extendiendo y mandando los parametros (super)
+) : Numeros(unoParametro, dosParametro) { // Extendiendo y mandando los parametros (super)
     init { // Bloque codigo constructor primario
         this.numeroDos
         this.numeroDos
     }
 
-    constructor(// Segundo constructor
+    constructor(
+// Segundo constructor
         uno: Int?, // Parametros
         dos: Int, // Parametros
-    ): this(
+    ) : this(
         if (uno == null) 0 else uno,
         dos
     )
 
-    constructor(// Tercer constructor
+    constructor(
+// Tercer constructor
         uno: Int, // Parametros
         dos: Int?, // Parametros
-    ): this(
+    ) : this(
         uno,
         if (dos == null) 0 else dos,
     )
+
+    constructor(
+// Cuarto constructor
+        uno: Int?, // parametros
+        dos: Int?, // parametros
+    ) : this( // llamda a constructor primario
+        if (uno == null) 0 else uno,
+        if (dos == null) 0 else dos
+    )
+
+    // public por defecto, o usar private o protected
+    public fun sumar(): Int {
+        val total = numeroUno + numeroDos
+        // Suma.agregarHistorial(total)
+        agregarHistorial(total)
+        return total
+    }
+
+    companion object { // Atributos y Metodos "Compartidos"
+        // entre las instancias
+        val pi = 3.14
+
+        fun elevarAlCuadrado(num: Int): Int {
+            return num * num
+        }
+
+        val historialSumas = arrayListOf<Int>()
+
+        fun agregarHistorial(valorNuevaSuma: Int) {
+            historialSumas.add(valorNuevaSuma)
+        }
+    }
 }
 
 // Void -> Unit
@@ -128,13 +229,13 @@ fun calcularSueldo(
     sueldo: Double, // Requerido
     tasa: Double = 12.00, // Opcional (defecto)
     bonoEspecial: Double? = null, //Opcion null -> nullable
-): Double{
+): Double {
     // Int -> Int? (nullable)
     // String -> String? (nullable)
     // Date -> Date? (nullable)
-    if (bonoEspecial == null){
-        return sueldo * (100/tasa)
+    if (bonoEspecial == null) {
+        return sueldo * (100 / tasa)
     } else {
-        return sueldo * (100/tasa) + bonoEspecial
+        return sueldo * (100 / tasa) + bonoEspecial
     }
 }
