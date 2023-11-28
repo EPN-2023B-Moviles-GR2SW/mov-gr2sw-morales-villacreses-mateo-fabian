a200bc5ca091b4841f909f123d6002c4ab110751
Mateo Morales <vimofama@outlook.com>
2023-11-28 07:33:56 -0500

    Add filter, any, all, and reduce operations to Main.kt

--- 01-KOTLIN/proyecto/src/main/kotlin/Main.kt
+++ 01-KOTLIN/proyecto/src/main/kotlin/Main.kt
@@ -123,6 +123,38 @@ fun main(args: Array<String>) {
 
     println(respuestaFilter)
     println(respuestaFilterDos)
+
+    // OR AND
+    // OR -> ANY (Alguno cumple?)
+    // AND -> ALL (Todos cumple?)
+
+    val respuestaAny: Boolean = arregloDinamico
+        .any{ valorActual: Int ->
+            return@any (valorActual > 5)
+        }
+    println(respuestaAny) // true
+
+    val respuestaAll: Boolean = arregloDinamico
+        .all { valorActual: Int ->
+            return@all (valorActual > 5)
+        }
+    println(respuestaAll) // false
+
+    // REDUCE -> Valor acumulado
+    // Valor acumulado = 0 (Siempre 0 en lenguaje Kotlin)
+    // [1, 2, 3, 4, 5] -> Sumeme todos los valores del arreglo
+    // valorIteracion1 = valorEmpieza + 1 = 0 + 1 = 1 -> Iteracion 1
+    // valorIteracion2 = valorIteracion1 + 2 = 1 + 2 = 3 -> Iteracion 2
+    // valorIteracion3 = valorIteracion2 + 3 = 3 + 3 = 6 -> Iteracion 3
+    // valorIteracion4 = valorIteracion3 + 4 = 6 + 4 = 10 -> Iteracion 4
+    // valorIteracion5 = valorIteracion4 + 5 = 10 + 5 = 15 -> Iteracion 5
+
+    val respuestaReduce: Int = arregloDinamico
+        .reduce { // acumulado = 0 -> SIEMPRE EMPIEZA EN 0
+            acumulado: Int, valorActual: Int ->
+            return@reduce (acumulado + valorActual) // -> Logica negocio
+        }
+    println(respuestaReduce)
 }
 
 abstract class NumerosJava {
@@ -187,7 +219,7 @@ class Suma(
     )
 
     constructor(
-// Cuarto constructor
+    // Cuarto constructor
         uno: Int?, // parametros
         dos: Int?, // parametros
     ) : this( // llamda a constructor primario
