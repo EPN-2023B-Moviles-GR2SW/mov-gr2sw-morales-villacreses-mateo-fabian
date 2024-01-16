9a8ea6d0019a8c7ee62ddda21b2eb1ac4f6bb3c3
Mateo Morales <vimofama@outlook.com>
2024-01-16 08:57:08 -0500

    Add RecyclerView and Like functionality

--- 02-Android/app/src/main/java/com/example/b2023gr2sw/FRecyclerView.kt
+++ 02-Android/app/src/main/java/com/example/b2023gr2sw/FRecyclerView.kt
@@ -2,10 +2,35 @@ package com.example.b2023gr2sw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
 
 class FRecyclerView : AppCompatActivity() {
+    var totalLikes = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frecycler_view)
+        inicializaraRecyclerView()
+    }
+
+    private fun inicializaraRecyclerView() {
+        val recyclerView = findViewById<RecyclerView>(R.id.rv_entrenadores)
+        val adaptador = FRecyclerViewAdaptadorNombreDescripcion(
+            this, // Contexto
+            BBaseDatosMemoria.arregloBEntrendador, // Arreglo datos,
+            recyclerView // Recycler view
+        )
+        recyclerView.adapter = adaptador
+        recyclerView.itemAnimator = DefaultItemAnimator()
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adaptador.notifyDataSetChanged()
+    }
+
+    fun aumentarTotalLikes() {
+        totalLikes += 1
+        val totalLikesTextView = findViewById<TextView>(R.id.tv_total_likes)
+        totalLikesTextView.text = totalLikes.toString()
     }
 }
